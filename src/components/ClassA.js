917b6a999016cb87972cab7e2d14f246218c441b
Kamil Kuziora <kamil@SHZ-DESKTOP.localdomain>
2020-06-01 00:19:41 +0200

    More content and routing

--- src/components/ClassA.js
+++ src/components/ClassA.js
@@ -1,6 +1,323 @@
 import React from "react";
 
 const ClassA = () => {
-  return <h1>HUMAN</h1>;
+  return (
+    <div id="content">
+      <h1>KLASA 1A - POL-HIS-WOS</h1>
+      <h2>Powinieneś wybrać klasę o takim nachyleniu przedmiotowym, jeśli:</h2>
+      <li>chcesz rozwijać zainteresowania naukami humanistycznymi;</li>
+      <li>
+        marzysz o podjęciu studi&oacute;w humanistycznych lub społecznych
+        (prawo, filologia polska, filozofia, dziennikarstwo, kulturoznawstwo,
+        stosunki międzynarodowe, europeistyka) na takich uczelniach jak:
+        Uniwersytet Jagielloński, Uniwersytet Warszawski, Uniwersytet
+        Rzeszowski, Uniwersytet Adama Mickiewicza w Poznaniu;
+      </li>
+      <li>
+        chcesz zmierzyć się z najlepszymi w Olimpiadzie Historycznej,
+        Olimpiadzie Literatury i Języka Polskiego, Olimpiadzie &bdquo;Losy
+        Żołnierza i Dzieje Oręża Polskiego&rdquo;;
+      </li>
+      <li>
+        interesują cię lekcje muzealne w Muzeum Powstania Warszawskiego, w
+        Muzeum Regionalnym w Stalowej Woli, w Międzynarodowym Centrum Edukacji o
+        Auschwitz i Holokauście;
+      </li>
+      <li>
+        zamierzasz przystąpić do egzaminu maturalnego z języka polskiego,
+        historii, wiedzy o społeczeństwie na poziomie rozszerzonym.
+      </li>
+
+      <table class="tg">
+        <thead>
+          <tr>
+            <th class="tg-t3" colspan="2">
+              Rok szkolny
+            </th>
+            <th class="tg-t2">2020/21</th>
+            <th class="tg-t2">2021/22</th>
+            <th class="tg-t2">2022/23</th>
+            <th class="tg-t2">2023/24</th>
+            <th class="tg-t7" rowspan="4">
+              Razem tyg.
+            </th>
+            <th class="tg-t8" rowspan="4">
+              liczba h
+            </th>
+          </tr>
+          <tr>
+            <td class="tg-t3" colspan="2">
+              Klasa
+            </td>
+            <td class="tg-t9">1</td>
+            <td class="tg-t9">2</td>
+            <td class="tg-t9">3</td>
+            <td class="tg-t9">4</td>
+          </tr>
+          <tr>
+            <td class="tg-t10" colspan="2">
+              Liczba tygodni
+            </td>
+            <td class="tg-t5">38</td>
+            <td class="tg-t5">38</td>
+            <td class="tg-t5">38</td>
+            <td class="tg-t5">38</td>
+          </tr>
+          <tr>
+            <td class="tg-t10" colspan="2">
+              Efektywna liczba tygodni
+            </td>
+            <td class="tg-t5">36</td>
+            <td class="tg-t5">36</td>
+            <td class="tg-t5">36</td>
+            <td class="tg-t5">36</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">j. polski</td>
+            <td class="tg-t11">4</td>
+            <td class="tg-t11">4</td>
+            <td class="tg-t11">4</td>
+            <td class="tg-t11">4</td>
+            <td class="tg-t12">16</td>
+            <td class="tg-t13">576</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">j. angielski</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t12">12</td>
+            <td class="tg-t13">432</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">j. niemiecki</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t12">8</td>
+            <td class="tg-t13">288</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">j. francuski</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t11">2 (1j)</td>
+            <td class="tg-t12">8</td>
+            <td class="tg-t13">288</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">matematyka</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">4</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">4</td>
+            <td class="tg-t12">14</td>
+            <td class="tg-t13">504</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">wf</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t11">3</td>
+            <td class="tg-t12">12</td>
+            <td class="tg-t13">432</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">gw</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t12">4</td>
+            <td class="tg-t13">144</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">biologia</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">4</td>
+            <td class="tg-t13">144</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">chemia</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">4</td>
+            <td class="tg-t13">144</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">geografia</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">4</td>
+            <td class="tg-t13">144</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">historia</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t12">8</td>
+            <td class="tg-t13">288</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">wos</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">2</td>
+            <td class="tg-t13">72</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">fizyka</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">4</td>
+            <td class="tg-t13">144</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">pp</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">2</td>
+            <td class="tg-t13">72</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">plastyka</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">1</td>
+            <td class="tg-t13">36</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">informatyka</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">3</td>
+            <td class="tg-t13">108</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">edb</td>
+            <td class="tg-t11">1</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">1</td>
+            <td class="tg-t13">36</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">religia</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t12">8</td>
+            <td class="tg-t13">288</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">wdżwr</td>
+            <td class="tg-t11">14r</td>
+            <td class="tg-t11">14r</td>
+            <td class="tg-t11">14r</td>
+            <td class="tg-t11">-</td>
+            <td class="tg-t12">1,11</td>
+            <td class="tg-t13">42</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">doradztwo zaw.</td>
+            <td class="tg-t11">2r</td>
+            <td class="tg-t11">2r</td>
+            <td class="tg-t11">3r</td>
+            <td class="tg-t11">3r</td>
+            <td class="tg-t12">0,26</td>
+            <td class="tg-t13">10</td>
+          </tr>
+          <tr>
+            <td class="tg-t1">1A</td>
+            <td class="tg-t1">j. polski R</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">2</td>
+            <td class="tg-t11">3+1</td>
+            <td class="tg-t12">9+1</td>
+            <td class="tg-t13">324+30</td>
+          </tr>
+          <tr>
+            <td class="tg-t14">1A</td>
+            <td class="tg-t14">wos R</td>
+            <td class="tg-t4">1</td>
+            <td class="tg-t4">2</td>
+            <td class="tg-t4">3</td>
+            <td class="tg-t4">3+1</td>
+            <td class="tg-t12">9+1</td>
+            <td class="tg-t13">324+30</td>
+          </tr>
+          <tr>
+            <td class="tg-t14">1A</td>
+            <td class="tg-t14">historia R</td>
+            <td class="tg-t4">1</td>
+            <td class="tg-t4">1</td>
+            <td class="tg-t4">2</td>
+            <td class="tg-t4">3+1</td>
+            <td class="tg-t12">7+1</td>
+            <td class="tg-t13">252+30</td>
+          </tr>
+          <tr>
+            <td class="tg-t14" colspan="2">
+              Razem
+            </td>
+            <td class="tg-t6">32,42</td>
+            <td class="tg-t6">36,42</td>
+            <td class="tg-t6">34,45</td>
+            <td class="tg-t6">30,08</td>
+            <td class="tg-t15">133,37+3</td>
+            <td class="tg-t16">4801,32+90</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default ClassA;
