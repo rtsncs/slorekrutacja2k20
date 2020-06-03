b16feace79131b93738c8f76c0ef06ce75e424e7
Dominik <dominik.b.v8@gmail.com>
2020-06-03 11:17:50 +0200

    smol

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -21,21 +21,27 @@ const Menu = () => {
           <div class="classes">
             <Link to="/class-a" class="kafle">
               1A
+              <span class="smol">POL-HIS-WOS</span>
             </Link>
             <Link class="kafle" to="/class-b">
               1B
+              <span class="smol">BIOL-CHEM-MAT</span>
             </Link>
             <Link class="kafle" to="/class-c">
               1C
+              <span class="smol">MAT-FIZ-INF</span>
             </Link>
             <Link class="kafle" to="/class-d">
               1D
+              <span class="smol">MAT-GEO-INF</span>
             </Link>
             <Link class="kafle" to="/class-e">
               1E
+              <span class="smol">MAT-GEO-ANG</span>
             </Link>
             <Link class="kafle" to="/class-f">
               1F
+              <span class="smol">GEO-BIOL-ANG</span>
             </Link>
           </div>
         </div>
